0128b9e70c6bcd257a2f7afc195831c7060e0233
mohamedshoaeb <shoaebmohamed56@gmail.com>
2022-12-28 15:59:49 -0800

    All tasks Done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poit
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char string[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+	return (0);
+}
