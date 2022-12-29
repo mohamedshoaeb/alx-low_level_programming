f8c1099fa7c6e1592c95a0e754db2f19f23007be
mohamedshoaeb <shoaebmohamed56@gmail.com>
2022-12-29 10:34:10 -0800

    first tsak

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 
 	/* my code*/
 	if (n > 0)
-		printf("%d is positive ", n);
+		printf("%d  is positive ", n);
 	else if (n < 0)
-		printf("%d is negative ", n);
+		printf("%d  is negative ", n);
 	else
-		printf("%d is zero ", n);
+		printf("%d  is zero ", n);
 
 	return (0);
 }
