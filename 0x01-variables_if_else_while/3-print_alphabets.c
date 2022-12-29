af6acd1f3616e568dba8f416419a82a8bf28fdaf
mohamedshoaeb <shoaebmohamed56@gmail.com>
2022-12-29 13:36:42 -0800

    Advanced Tasks

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,22 +3,21 @@
 /**
  * main - entry point
  *
- * Description: 'print alphabet in lowercase then in uppercase'
+ * Description : print [a-z] , [A-Z] lphabet in lowercase then in uppercase
  *
  * Retuen: Always 0
  */
-
 int main(void)
 {
 	char ch = 'a';
 	char CH = 'A';
-
+	/*MY code 1 */
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-
+	/*My code 2 */
 	while (CH <= 'Z')
 	{
 		putchar(CH);
