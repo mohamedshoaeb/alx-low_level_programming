91d119b2d569fd5a893adaaffb53aba0214b4b71
mohamedshoaeb <shoaebmohamed56@gmail.com>
2023-01-02 09:30:08 -0800

    All Taskes done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,14 +12,18 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
-	
-	while (op == 0)
+	int i = 0, r;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+		if (s1[i] != s2[i])
+		{
+			r = s1[i] - s2[i];
 			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		}
+		else
+			r = s1[i] - s2[i];
+		i++
 	}
-	return (op);
+	return (r);
 }
