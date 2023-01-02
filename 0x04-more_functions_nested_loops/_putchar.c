ec78cc28a71aeda6249c5156c8d4d6dfddbdf6f7
mohamedshoaeb <shoaebmohamed56@gmail.com>
2023-01-02 06:14:49 -0800

    Tasks

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
-#include "main.h"
 
 /**
- * _putchar - write char
+ * _putchar - writes the character
+ * 
+ * @c: The character to print
  *
- * Return : Alwyes 1 in succsess
+ * Return: On success 1
  */
 
 int _putchar(char c)
 {
-	return (write(1,&c,1));
+	return (write(1, &c, 1));
 }
