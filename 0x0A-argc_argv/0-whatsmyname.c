ea83ea7d4ddf88e2c5fe0919d7db7b7f9ad68068
mohamedshoaeb <shoaebmohamed56@gmail.com>
2023-01-08 06:13:21 -0800

    All Taskes Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * @argc: number of argument
+ * @argv: array of arguments
+ *
+ * Return: Alwys is 0
+ */
+
+int main(int __attribute__((unused)) argc, char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguments
+ *
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ *
+ * Return: Always 0 
+ *
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
