693ecf824734f18dddea09a188b79b82b382a7e5
mohamedshoaeb <shoaebmohamed56@gmail.com>
2023-01-09 05:52:05 -0800

    First Task

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ * _isupper - uppercase letters
+ *
+ * @c: char to commcd*
+ *
+ * Return: 0 or 
+ */
+
+int _isupper(int c)
+
+{
+
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
